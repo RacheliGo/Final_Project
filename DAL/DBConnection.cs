fa8269e1f78840058bd839700d81cede5b892720
RacheliGo <racheli5910@gmail.com>
2024-07-23 21:16:31 +0300

    Add project files.

---
+++ DAL/DBConnection.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DBConnection
+    {
+        InterviewsManagerContext InterviewsManagerEntities= new InterviewsManagerContext();
+        public List<Candidate> GetCandidates()
+        {
+            return InterviewsManagerEntities.Candidates.ToList();
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return InterviewsManagerEntities.Employees.ToList();
+        }
+
+        public List<Interview> GetInterviews()
+        {
+            return InterviewsManagerEntities.Interviews.ToList();
+        }
+    }
+}
---
+++ EmployeesManagerApp/HOME.xaml.cs
@@ -0,0 +1,44 @@
+﻿using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for HOME.xaml
+    /// </summary>
+    public partial class HOME : Window
+    {
+        InterviewsMBL interviewsMBL;
+        public HOME()
+        {
+            InitializeComponent();
+
+            interviewsMBL = new InterviewsMBL();
+        }
+
+        private void ButtonEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.ShowDialog();
+        }
+
+        private void ButtonCandidate_Click(object sender, RoutedEventArgs e)
+        {
+            interviewsMBL.GetInterviewsDetails();
+            FindCandidate findCandidate = new FindCandidate();
+            findCandidate.ShowDialog();
+        }
+    }
+}
